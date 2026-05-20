1e89a23fba495265347b66a4ddf494b0b91162a2
Kacper <Kacper.lapinski21@gmail.com>
2026-05-20 10:52:35 +0200

    Initialize project

---
+++ klinika-app/src/main/java/pl/klinika/KlinikaAppApplication.java
@@ -0,0 +1,13 @@
+package pl.klinika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlinikaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlinikaAppApplication.class, args);
+	}
+
+}
---
+++ klinika-app/src/test/java/pl/klinika/KlinikaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klinika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlinikaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
